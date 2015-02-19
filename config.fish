f4fe875e4ec291856b2da92899aed6e9b5980dcd
Nick Burns <nick@burns.io>
2015-02-18 22:12:30 -0800

    colors+vim

--- config.fish
+++ config.fish
@@ -44,3 +44,22 @@ set -g -x LESS '-gFERXP%lB$'
 set -g -x LESSOPEN '|pygmentize -g'
 
 set -g -x ACK_PAGER_COLOR $PAGER
+
+set -g -x VIM_APP_DIR /opt/homebrew-cask/Caskroom/macvim/7.4-73/MacVim-snapshot-73
+
+set -g -x base03 002b36
+set -g -x base02 073642
+set -g -x base01 586e75
+set -g -x base00 657b83
+set -g -x base0 839496
+set -g -x base1 93a1a1
+set -g -x base2 eee8d5
+set -g -x base3 fdf6e3
+set -g -x yellow b58900
+set -g -x orange cb4b16
+set -g -x red dc322f
+set -g -x magenta d33682
+set -g -x violet 6c71c4
+set -g -x blue 268bd2
+set -g -x cyan 2aa198
+set -g -x green 859900
