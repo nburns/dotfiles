1709f9cad73056b49e45b7ab1dc9a868f10392ca
Nick Burns <nick@burns.io>
2015-04-07 19:06:34 -0700

    loading

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-if status --is-interactive
+if [ "$_SETUPDONE" != 'true' ] ; or status --is-login
     function error
         echo $argv > /dev/stderr
     end
@@ -6,29 +6,25 @@ if status --is-interactive
     function prepend_to_path
         for arg in $argv
             if [ -d $arg ]
-                set -g -x PATH $argv $PATH
+                set -g -x PATH $arg $PATH
             end
         end
     end
 
-    prepend_to_path\
-        ~/bin\
-        /usr/local/texlive/2014/bin/x86_64-darwin\
-        /usr/local/bin\
-        ~/.cabal/bin\
-        /opt/X11/bin/
+    prepend_to_path ~/bin\
+                    /usr/local/bin\
+                    ~/.cabal/bin\
+                    /opt/X11/bin\
+                    /usr/local/texlive/2014/bin/x86_64-darwin
     
     switch (uname)
         case 'Darwin'
             set -g -x LC_ALL 'en_US.UTF-8'
             set -g -x LANG 'en_US.UTF-8'
-            
             set -g -x PYTHONDONTWRITEBYTECODE 'True'
-            
-            set -g -x VIM_APP_DIR /opt/homebrew-cask/Caskroom/macvim/7.4-73/MacVim-snapshot-73
-
             set -g -x HOMEBREW_NO_EMOJI 1
             set -g -x HOMEBREW_CASK_OPTS '--appdir=/Applications'
+            set -g -x VIM_APP_DIR (dirname (readlink /Applications/MacVim.app))
     end
 
     if [ (which mvim) ]
@@ -40,7 +36,7 @@ if status --is-interactive
     
     if [ -d ~/bin/virtualfish ]
         pushd ~/bin/virtualfish
-        eval (python -m virtualfish compat_aliases)
+        eval (python -m virtualfish compat_aliases auto_activation)
         popd
     else
         error 'could not find ~/.virtualfish'
@@ -65,8 +61,9 @@ if status --is-interactive
     function fish_right_prompt
         set last $status
         if [ $last -ne 0 ]
-            set_color red; echo -n $last; set_color normal
+            echo -n $last
         end
     end
     
+    set -g -x _SETUPDONE 'true'
 end
